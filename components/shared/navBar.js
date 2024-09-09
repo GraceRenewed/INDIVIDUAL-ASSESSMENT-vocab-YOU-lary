3ce577413720b6313def8693fcb17aec55991fff
Christina Vieau <cvieau@live.com>
2024-09-09 18:38:32 -0400

    changed navBar id to all-vocab

--- components/shared/navBar.js
+++ components/shared/navBar.js
@@ -11,7 +11,7 @@ const navBar = () => {
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item active">
-              <a class="nav-link" href="#" id="all-cards">
+              <a class="nav-link" href="#" id="all-vocab">
                 All Cards <span class="sr-only">(current)</span>
               </a>
             </li>
